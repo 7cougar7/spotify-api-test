a0c269360a046abc1c65f957dbcf9e5ae5d37ff1
7cougar7 <tilogarcia1@gmail.com>
2021-05-23 01:52:18 -0500

    Added menu options and documentations

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from spotipy.oauth2 import SpotifyOAuth
 SONGS_SENT_PER_REQUEST = 95  # Must be less than 100
 MAX_SONGS_DIRECTLY_REQUESTED = 300
 NAME_PREFIX = "Spotized - "
+USER_INPUT_PROMPT = '\n>> '
 SCOPE = "user-library-read, " \
         "streaming, " \
         "user-follow-read, " \
@@ -23,14 +24,14 @@ SCOPE = "user-library-read, " \
 def get_artist(sp: spotipy.client.Spotify):
     search_items = []
     while len(search_items) == 0:
-        requested_artist = input('What artist would you like to make a playlist for: ')
+        requested_artist = input('What artist would you like to make a playlist for:' + USER_INPUT_PROMPT)
         search_result = sp.search('artist:' + requested_artist, type='artist')
         search_items = search_result['artists']['items']
         if len(search_items) == 0:
             print("**  Unable to find desired artist in the given search **")
     if len(search_items) > 1:
         for artist in search_items:
-            response = input("Were you looking for " + artist['name'] + "? (y/n/q) ")
+            response = input("Were you looking for " + artist['name'] + "? (y/n/q)" + USER_INPUT_PROMPT)
             if response.lower() == 'y' or response.lower() == '1':
                 return artist
             if response.lower() == 'q':
@@ -51,7 +52,8 @@ def get_artists(sp: spotipy.client.Spotify):
 
     finished_adding_artists = False
     while not finished_adding_artists:
-        done_adding_response = input("Would you like to add more artists to base your playlist off? (y/n) ")
+        done_adding_response = input(
+            "Would you like to add more artists to base your playlist off? (y/n)" + USER_INPUT_PROMPT)
         if done_adding_response.lower() == 'y':
             artist = get_artist(sp)
             if artist and artist not in all_artists:
@@ -60,7 +62,8 @@ def get_artists(sp: spotipy.client.Spotify):
             finished_adding_artists = True
 
     print(
-        ">> Playlist will be based on the following artist(s): " + ", ".join(get_list_properties(all_artists, 'name'))
+        "** Playlist will be based on the following artist(s): " +
+        ", ".join(get_list_properties(all_artists, 'name')) + " **"
     )
     return all_artists
 
@@ -70,7 +73,8 @@ def get_time_length_songs(sp: spotipy.client.Spotify, artists: list):
     amount_of_time = 90
     while not successful_time_input:
         try:
-            amount_of_time = int(input("How long would you like your playlist to be? Enter in whole minutes: "))
+            amount_of_time = int(
+                input("How long would you like your playlist to be? Enter in whole minutes:" + USER_INPUT_PROMPT))
             successful_time_input = True
         except ValueError:
             pass
@@ -98,7 +102,7 @@ def is_device_available(sp: spotipy.client.Spotify):
 def wait_for_active_device(sp: spotipy.client.Spotify):
     while not is_device_available(sp):
         print("Please launch Spotify on an app and initiate playback on that device")
-        retry_response = input("Press any key to attempt playing playlist or \"q\" to quit")
+        retry_response = input("Press enter to attempt playing playlist or \"q\" to quit" + USER_INPUT_PROMPT)
         if retry_response.lower() == 'q':
             exit(0)
 
@@ -111,7 +115,7 @@ def create_new_playlist(sp: spotipy.client.Spotify):
     artists = get_artists(sp)
     artist_names = get_list_properties(artists, 'name')
     artist_uris = get_list_properties(artists, 'uri')
-    duration_unit = input("Would you prefer to specify an amount of time or number of songs? (1/2) ")
+    duration_unit = input("Would you prefer to specify an amount of time or number of songs? (1/2)" + USER_INPUT_PROMPT)
     if duration_unit == '1':
         tracks_to_insert = get_time_length_songs(sp, artist_uris)
     else:
@@ -122,7 +126,7 @@ def create_new_playlist(sp: spotipy.client.Spotify):
                 num_tracks = int(
                     input(
                         "How many songs would you like in your playlist? (Max out at " +
-                        str(MAX_SONGS_DIRECTLY_REQUESTED) + ") Enter whole number of songs: "
+                        str(MAX_SONGS_DIRECTLY_REQUESTED) + ") Enter whole number of songs:" + USER_INPUT_PROMPT
                     )
                 )
                 successful_number_of_tracks = True
@@ -149,13 +153,13 @@ def create_new_playlist(sp: spotipy.client.Spotify):
 
     playlist_items = sp.playlist_items(playlist_id=playlist['id'])['items']
     playlist_duration = sum([x['track']['duration_ms'] for x in playlist_items]) // 60000
-    duration_text = '' if playlist_duration // 3600 == 0 else str(playlist_duration / 3600) + ' hours, '
-    duration_text += str(playlist_duration % 3600) + ' minutes'
-    print('>> Playlist is ' + duration_text + '(' + str(len(playlist_items)) + ' songs) long')
-    start_response = input('Would you like to listen to your newly created playlist? (y/n) ')
+    duration_text = '' if playlist_duration // 60 == 0 else str(playlist_duration // 60) + ' hours, '
+    duration_text += str(playlist_duration % 60) + ' minutes'
+    print('** Playlist is ' + duration_text + '(' + str(len(playlist_items)) + ' songs) long **')
+    start_response = input('Would you like to listen to your newly created playlist? (y/n)' + USER_INPUT_PROMPT)
     if start_response.lower() == 'n':
         return
-    shuffle_response = input('Would you like to shuffle the music? (y/n) ')
+    shuffle_response = input('Would you like to shuffle the music? (y/n)' + USER_INPUT_PROMPT)
     wait_for_active_device(sp)
     sp.shuffle(shuffle_response.lower() == 'y')
     sp.start_playback(context_uri=playlist['uri'])
@@ -179,7 +183,7 @@ def delete_automated_playlists(sp: spotipy.client.Spotify):
     while not taken_action:
         confirmation_response = input(
             "Confirm that these playlists should be permanently "
-            "DELETED by typing \"Delete All Playlists\" or \"q\" to quit \n>> "
+            "DELETED by typing \"Delete All Playlists\" or \"q\" to quit" + USER_INPUT_PROMPT
         )
 
         if confirmation_response == 'Delete All Playlists':
@@ -195,6 +199,17 @@ def delete_automated_playlists(sp: spotipy.client.Spotify):
 if __name__ == '__main__':
     load_dotenv()
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=SCOPE))
-    # create_new_playlist(sp)
-    delete_automated_playlists(sp)
-    input("Press Enter to Quit")
+    while True:
+        menu_response = input(
+            "Welcome to Spotized!\n"
+            "Type \"1\" to create a new playlist\n"
+            "Type \"2\" to delete all playlists created\n"
+            "Type \"q\" to quit" +
+            USER_INPUT_PROMPT
+        )
+        if menu_response == '1':
+            create_new_playlist(sp)
+        elif menu_response == '2':
+            delete_automated_playlists(sp)
+        elif menu_response == 'q':
+            exit(0)
---
+++ readme.txt
@@ -0,0 +1,2 @@
+To install development requirements: pip install -r requirements.txt
+To create one file executable(output to dist/): pyinstaller --onefile main.py
